df140457c86e200d1a8f97d2f15ec8116c2fe914
Jonian Guveli <jonian@hardpixel.eu>
2023-12-26 18:27:04 +0200

    add support for components in markdown

--- src/builder/parsers/markdown/index.js
+++ src/builder/parsers/markdown/index.js
@@ -1,5 +1,102 @@
 import MarkdownIt from 'markdown-it'
 import matter from 'gray-matter'
+import { DOMParser } from '@xmldom/xmldom'
+
+import JSON5 from 'json5'
+import htmlTags from 'html-tags'
+import voidHtmlTags from 'html-tags/void.js'
+
+const isHtmlTag = tag => htmlTags.includes(tag) || voidHtmlTags.includes(tag)
+
+const htmlToText = html => {
+  const dom = new DOMParser()
+  const doc = dom.parseFromString(`<!DOCTYPE html><html><body>${html}</body></html>`)
+
+  return nodeToText(doc.documentElement)
+}
+
+const nodeToText = (node) => {
+  if (node.nodeType === 3 || node.nodeType === 4) {
+    return node.nodeValue
+  }
+
+  let result = ''
+
+  if (node.childNodes && node.childNodes.length > 0) {
+    for (let i = 0; i < node.childNodes.length; i++) {
+      const child = node.childNodes[i]
+      result += nodeToText(child)
+    }
+  }
+
+  return result
+}
+
+const htmlToVnode = html => {
+  const dom = new DOMParser()
+  const doc = dom.parseFromString(`<!DOCTYPE html><html><body>${html}</body></html>`)
+
+  const env = { tags: [] }
+  const arr = nodeToVnode(doc.documentElement, env)
+
+  if (env.tags.every(isHtmlTag)) {
+    return html
+  } else {
+    return arr
+  }
+}
+
+const nodeToVnode = (node, env) => {
+  if (node.nodeType === 3 || node.nodeType === 4) {
+    return node.nodeValue
+  }
+
+  const children = []
+
+  if (node.childNodes && node.childNodes.length > 0) {
+    for (let i = 0; i < node.childNodes.length; i++) {
+      const child = node.childNodes[i]
+      children.push(nodeToVnode(child, env))
+    }
+  }
+
+  if (node.nodeName == 'html') return children[0]
+  if (node.nodeName == 'body') return children
+
+  const attributes = {}
+
+  if (node.attributes && node.attributes.length > 0) {
+    for (let i = 0; i < node.attributes.length; i++) {
+      const attribute = node.attributes[i]
+
+      try {
+        attributes[attribute.name] = JSON5.parse(attribute.value)
+      } catch (e) {
+        attributes[attribute.name] = attribute.value
+      }
+    }
+  }
+
+  const result = [node.nodeName]
+
+  if (Object.keys(attributes).length) {
+    result.push(attributes)
+  }
+
+  if (children.length) {
+    if (children.length == 1 && typeof children[0] == 'string') {
+      result.push(children[0])
+    } else {
+      result.push(children)
+    }
+  }
+
+  if (!env.tags.includes(node.nodeName)) {
+    env.tags.push(node.nodeName)
+  }
+
+  return result
+}
 
 const cleanHTML = html => {
   return html.trim()
@@ -25,12 +122,13 @@ export default class Markdown {
       excerpt_separator: '<!--more-->'
     })
 
-    const body    = cleanHTML(markdown.render(content || ''))
-    const excerpt = cleanHTML(markdown.render(meta.excerpt || ''))
+    const excerptHTML = cleanHTML(markdown.render(meta.excerpt || ''))
+    const contentHTML = cleanHTML(markdown.render(content || ''))
 
     return {
-      excerpt,
-      body,
+      description: htmlToText(excerptHTML),
+      excerpt: htmlToVnode(excerptHTML),
+      body: htmlToVnode(contentHTML),
       ...data
     }
   }
--- src/runtime/components/NuxtContent.vue
+++ src/runtime/components/NuxtContent.vue
@@ -1,14 +1,41 @@
-<template>
-  <div v-html="document.body" />
-</template>
-
 <script>
+import { h, resolveDynamicComponent } from 'vue'
+import { NuxtLink } from '#components'
+
+const isArr  = Array.isArray
+const isObj  = item => typeof item == 'object'
+const isPobj = item => isObj(item) && !isArr(item)
+
+const toNode = node => isArr(node) ? toComp(node) : node
+const toArgs = item => isArr(item) ? item.map(toNode) : item
+const toSlot = item => isPobj(item) ? item : (() => item)
+
+const toComp = ([tag, ...props]) => {
+  const comp = /nuxt-?link/i.test(tag) ? NuxtLink : resolveDynamicComponent(tag)
+  const args = isObj(comp) ? props.map(toArgs).map(toSlot) : props.map(toArgs)
+
+  return h(comp, ...args)
+}
+
 export default {
   name: 'NuxtContent',
   props: {
+    tag: {
+      type: String,
+      default: 'div'
+    },
     document: {
       type: Object,
       required: true
+    }
+  },
+  render() {
+    const body = this.document.body
+
+    if (isArr(body)) {
+      return h(this.tag, body.map(toNode))
+    } else {
+      return h(this.tag, { innerHTML: body })
     }
   }
 }
